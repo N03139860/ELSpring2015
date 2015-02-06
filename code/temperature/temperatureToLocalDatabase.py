18db83cc58a365d99264760e00ec347395f3c538
kikocastro <castro.112358@gmail.com>
2015-02-06 01:23:52 -0500

    local database writing

--- code/temperature/temperatureToLocalDatabase.py
+++ code/temperature/temperatureToLocalDatabase.py
@@ -5,7 +5,7 @@ import temperatureDatabase
 temperature = temperature.Temperature()
 listOfTemperatures = temperature.getListOfTemperatures(20, 5)
 
-db = temperatureDatabase.TemperatureDatabase('../misc/local.db')
+db = temperatureDatabase.TemperatureDatabase('../../misc/local.db')
 
 for list in listOfTemperatures:
   db.dataEntry(list)
