44eb0d6e581f530d958b4d00c89880e0506fc900
Frederico Castro <castro.112358@gmail.com>
2015-02-12 22:55:12 -0500

    query working but json not right

--- misc/website/pages/index.php
+++ misc/website/pages/index.php
@@ -267,7 +267,6 @@
 
     <!-- jQuery -->
     <script src="../bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="../js/jquery.binarytransport.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
